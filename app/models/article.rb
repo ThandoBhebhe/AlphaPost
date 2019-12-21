60564f1ed69702259e3966336f3a137e19f5b078
ThandoBhebhe <bukhosi95@gmail.com>
2019-12-21 18:22:11 +0000

    created articles table and validates on DB

---
+++ app/models/article.rb
@@ -0,0 +1,8 @@
+class Article < ApplicationRecord
+
+validates :title, presence: true, length: {minimum: 3, maximum: 20}
+validates :description, presence: true, length: {minimum: 3, maximum: 20}
+
+
+
+end
