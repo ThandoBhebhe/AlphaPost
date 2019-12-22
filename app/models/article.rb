97a27b4111e897580895fc5e898b58d783f28760
ThandoBhebhe <bukhosi95@gmail.com>
2019-12-22 13:37:15 +0000

    added a form, can save UI input to database

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,6 @@
 class Article < ApplicationRecord
 
-validates :title, presence: true, length: {minimum: 3, maximum: 20}
-validates :description, presence: true, length: {minimum: 3, maximum: 20}
-
-
+  validates :title, presence: true, length: {minimum: 3, maximum: 20}
+  validates :description, presence: true, length: {minimum: 3, maximum: 20}
 
 end
