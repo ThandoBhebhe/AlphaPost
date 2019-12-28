35a642f6f8aa92a294e91aaeb525a70f96e7b572
ThandoBhebhe <bukhosi95@gmail.com>
2019-12-28 16:49:57 +0000

    stable controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,12 +4,13 @@ def new
   @article = Article.new
 end
 def create
-  # render plain: params[:article].inspect
+  # render plain: params[:article].inspect #display passed in values
   @article = Article.new(article_values)
   @article.save
   redirect_to articles_show(@article)
 end
 
+private
 def article_values
   params.require(:article).permit(:title, :description)
 end
